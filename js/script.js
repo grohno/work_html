944207793ad3a08c2481986cd84f91f8c90aed6d
yuki ohno <jeanadler33@gmail.com>
2021-03-17 07:04:19 +0000

    JS追加(送信ボタン色変更)

--- js/script.js
+++ js/script.js
@@ -3,4 +3,10 @@ $(function(){
   $('#accordion p.open').on('click', function(){
     $(this).next('form').slideToggle();
   });
+  $('.btn input').on('mouseenter', function(){
+    $(this).css('background-color', 'yellowgreen');
+  })
+  .on('mouseleave', function(){
+    $(this).css('background-color', '#ea5550');
+  });
 });
